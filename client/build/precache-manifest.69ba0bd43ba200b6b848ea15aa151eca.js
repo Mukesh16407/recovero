844e7363a98ca44bb59b51463a5323dd950e15a7
Mukesh16407 <mukesh.16407@gmail.com>
2022-12-08 14:38:55 +0530

    fix-vercel-problem

---
+++ client/build/precache-manifest.69ba0bd43ba200b6b848ea15aa151eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebc074833500539c1ca1",
+    "url": "/static/css/main.81ffaa15.chunk.css"
+  },
+  {
+    "revision": "ebc074833500539c1ca1",
+    "url": "/static/js/main.ebc07483.chunk.js"
+  },
+  {
+    "revision": "36e27f840bb155b80054",
+    "url": "/static/js/1.36e27f84.chunk.js"
+  },
+  {
+    "revision": "3784e57199f81d95a458",
+    "url": "/static/js/2.3784e571.chunk.js"
+  },
+  {
+    "revision": "0294bf54155143bc935f",
+    "url": "/static/js/runtime~main.0294bf54.js"
+  },
+  {
+    "revision": "ec0ba40b0694d7160f0b8869c3da1fdc",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const connect = require("./config/dbConfig");
 
 require("dotenv").config();
+const path = require("path");
+
 app.use(express.json());
 
 const userRoute = require("./routes/userRoute");
@@ -10,6 +12,16 @@ const userRoute = require("./routes/userRoute");
 app.use("/api/user", userRoute);
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => {
     connect()
